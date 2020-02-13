b8f832b574ef7193398c5a51cc2e5d662fac044c
Siddiqui <masiddiqui91@gmail.com>
2020-02-13 22:44:59 +0000

    cleanup

--- vue/main.js
+++ vue/main.js
@@ -1,10 +1,4 @@
 import Vue from "vue";
-import App from "./App";
-
-// Vue.component(
-//   "users-count",
-//   import(/* webpackChunkName: "about" */ "./components/UsersCount.vue")
-// );
 
 Vue.component("users-count", function(resolve) {
   require(["./components/UsersCount"], resolve);
